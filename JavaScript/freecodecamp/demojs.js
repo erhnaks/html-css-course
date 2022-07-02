e07df017334646600314e2294af6fdfed13f3f02
erhnaks <erhnaks@hotmail.com>
2022-07-02 13:50:02 +0100

    freecodecamp javaScript tutorials

---
+++ JavaScript/freecodecamp/demojs.js
@@ -0,0 +1,23 @@
+// This line will print to console;
+console.log("hello World");
+// Basic operations
+console.log(6 * 7);
+
+// Basic addition operation;
+var num1 = 4;
+var num2 = 5;
+
+console.log(num1 + num2); // Print the result to the console
+//Declaring Variable
+var myName = "Erhan";
+console.log(myName);
+//Assign variables and then use operation method
+
+var a;
+var b = 3;
+
+a = 5;
+
+var c = a * b;
+
+console.log(c);
