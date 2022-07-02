69d04a4ef6fbb182df215d96ceec1efec6dd1364
erhnaks <erhnaks@hotmail.com>
2022-07-02 12:36:11 +0100

    jscript functions

--- JavaScript/W3SchoolJavaScript/jScript-SRC/script.js
+++ JavaScript/W3SchoolJavaScript/jScript-SRC/script.js
@@ -1,4 +1,12 @@
-<script>
-  type = button onclick = 'getElementById("demo1").innerHTML = "Hello did you
-  notice the change?"
-</script>;
+function a() {
+  document.getElementById("demo1").innerHTML =
+    "Hello did you  notice the change?";
+}
+
+function b() {
+  document.getElementById("demo2").innerHTML = " You have Double clicked  me!";
+}
+
+function r() {
+  document.getElementById("reset").innerHTML = "page resetted";
+}
