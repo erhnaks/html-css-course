f9f927a144872632b4b55202ea90453eae33c9d3
erhnaks <erhnaks@hotmail.com>
2022-07-02 15:18:02 +0100

    Modifiying Arrays with Index

--- JavaScript/freecodecamp/arrayList.js
+++ JavaScript/freecodecamp/arrayList.js
@@ -11,3 +11,11 @@ var nums = [20, 30, 40, 50];
 
 var indexnum = nums[0];
 console.log(indexnum); //result is 20 because it is index 0 and 30 is index of 1;
+
+//Modify Arrays with index
+
+var modifyArray = [10, 20, 30, 50]; //lets modify 50 and insert 40;
+
+modifyArray[3] = 40;
+
+console.log(modifyArray);
