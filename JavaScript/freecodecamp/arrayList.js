34a356ee770d2ba1368d86d49b6d8cb362bf0fa3
erhnaks <erhnaks@hotmail.com>
2022-07-02 15:15:38 +0100

    ArrayList in Js

---
+++ JavaScript/freecodecamp/arrayList.js
@@ -0,0 +1,13 @@
+var myArray = [
+  ["John", "Doe", 32],
+  ["Alice", "Alisson", 28],
+];
+
+console.log(myArray);
+
+//finding indexes in Arrays
+
+var nums = [20, 30, 40, 50];
+
+var indexnum = nums[0];
+console.log(indexnum); //result is 20 because it is index 0 and 30 is index of 1;
